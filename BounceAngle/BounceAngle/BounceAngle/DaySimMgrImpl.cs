de3664958178b3e79b37b63d60b31717ce2110fe
Kevin Kruusi <kkruusi@gmail.com>
2012-05-12 16:31:42 -0400

    add run sim

--- BounceAngle/BounceAngle/BounceAngle/DaySimMgrImpl.cs
+++ BounceAngle/BounceAngle/BounceAngle/DaySimMgrImpl.cs
@@ -33,12 +33,14 @@ namespace BounceAngle
             int _food = 0;
             int _aliveSurvivors = 0;
             int _time = 0;
+            int _zombies = 0;
             // pop up the ui with items scavenged, update all stats, get ready to go to night mode
             foreach(BuildingData data in buildingsToScavenge){
                 _ammo += data.getAmmo();
                 _food += data.getFood();
                 _aliveSurvivors += data.getSurvivors();
                 _ammo -= data.getZombies();
+                _zombies += data.getZombies();
                 if (data.getScavengeTime() > _time) {
                     //the time is always the building that took the longest to scavenge.
                     //for our simple implementation.
@@ -49,9 +51,9 @@ namespace BounceAngle
                 food += _food;
                 aliveSurvivors += _aliveSurvivors;
 
-                DayGameEngineImp.getGameEngine().getMenuManager().setAmmo(ammo);
-                DayGameEngineImp.getGameEngine().getMenuManager().setFood(food);
-                DayGameEngineImp.getGameEngine().getMenuManager().setTime(time + 12 * 60);
+                DayGameEngineImp.getGameEngine().getMenuManager().SetUIAmmo = ammo;
+                DayGameEngineImp.getGameEngine().getMenuManager().SetUIFood = food;
+                DayGameEngineImp.getGameEngine().getMenuManager().SetUITime = time + 12 * 60;
         
                 data.setAmmo((int)data.getAmmo() / 2 + random.Next(1));
                 data.setFood((int)data.getFood() / 2 + random.Next(1));
@@ -59,10 +61,9 @@ namespace BounceAngle
                 data.setZombies((int)data.getZombies() / 2 + random.Next(1));
             
             }
-            //TODO: pop up window
              // use _ammo & _food & _aliveSurvivors for the sub total of the day
-             // ok button
             Console.WriteLine("Food gathered: " + _food + " Ammo gathered: " + _ammo + " Survivors Found: " + _aliveSurvivors);
+            DayGameEngineImp.getGameEngine().getMenuManager().displaySummary(_food, _ammo, _zombies, _aliveSurvivors);
 
             //clear the buildings scavenged this turn
             buildingsToScavenge = new List<BuildingData>();
