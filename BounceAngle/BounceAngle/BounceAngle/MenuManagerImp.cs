526229b02e810baa03fd2784bad0e3287af67e81
unknown <Graphic@510-10.(none)>
2012-05-13 14:49:38 -0400

    Menu Safe House Check

--- BounceAngle/BounceAngle/BounceAngle/MenuManagerImp.cs
+++ BounceAngle/BounceAngle/BounceAngle/MenuManagerImp.cs
@@ -185,7 +185,11 @@ namespace BounceAngle
             menuItems[3].Text[0] = data.getBuildingDescription();
             menuItems[3].popUpHeader();
             menuItems[3].PopUpImg = data.getTexture();
-            string[] subText = {"Suvivors: " + data.getSurvivors(), "Food: " +data.getFood(), "Ammo: " +data.getAmmo(), "Scavenge Time: " +data.getScavengeTime()};
+            string[] subText = { "Suvivors: " + data.getSurvivors(), "Food: " + data.getFood(), "Ammo: " + data.getAmmo(), "Scavenge Time: " + data.getScavengeTime() };
+            if (data.isSafehouse())
+            {
+                subText = new string[] { "SafeHouse: No Supplies" };
+            }
             menuItems[3].popUpSub(popUpSubText, subText);
         }
 
