b38481e5105842b35712c48bfa44b0c53859613b
Jared Malcolmson <malcolmson@live.com>
2012-05-12 22:28:04 -0400

    fun candy canes

--- BounceAngle/BounceAngle/BounceAngle/ZombieManagerImplementation.cs
+++ BounceAngle/BounceAngle/BounceAngle/ZombieManagerImplementation.cs
@@ -13,7 +13,7 @@ namespace BounceAngle
         public List<ZombieData> Zombies { get; set; }
         public List<Texture2D> zombieTextures { get; set; }
         private Random random = new Random();
-        private float rotation { get; set; }
+        //private float rotation { get; set; }
 
         public ZombieManagerImplementation()
         {
@@ -52,44 +52,43 @@ namespace BounceAngle
             SurvivorManager survivors = NightGameEngineImp.getGameEngine().getSurvivorManager();
             for (int i = 0; i < Zombies.Count; i++)
             {
+
                 for (int j = 0; j < survivors.getAllSurvivors().Count; j++){
-                    if ((Zombies[i].getCurrentLocation() - survivors.getSurvivorById(j).getCurrentLocation()).Length() < 150
+                    if ((Zombies[i].getCurrentLocation() - survivors.getSurvivorById(j).getCurrentLocation()).Length() < 350
                         && (Zombies[i].getCurrentLocation() - survivors.getSurvivorById(j).getCurrentLocation()).Length() > 30)
                     {
                         Zombies[i].setDestination(survivors.getSurvivorById(j).getCurrentLocation());
-                    }    
+                    }
+                    //TODO: "Hit" detection
+                    if ((Zombies[i].getCurrentLocation() - survivors.getSurvivorById(j).getCurrentLocation()).Length() < 20)
+                    {
+                        Zombies.RemoveAt(i);
+                    }
                 }
-                List<Building> buildings = NightGameEngineImp.getGameEngine().getMapManager().getAllBuildings();
-               
-                if (Zombies[i].getDestination() != null)
+                try
                 {
                     Vector2 zombieToMove = Zombies[i].getDestination() - Zombies[i].getCurrentLocation();
                     zombieToMove.Normalize();
-                    Zombies[i].setCurrentLocation(Zombies[i].getCurrentLocation() + zombieToMove * Zombies[i].getMoveSpeed());
-                    rotation = (float)Math.Atan2(Zombies[i].getDestination().Y - Zombies[i].getCurrentLocation().Y, Zombies[i].getDestination().X - Zombies[i].getCurrentLocation().X);
-                    return;
-                }
-                for (int j = 0; j < buildings.Count; j++)
-                {
-                    if (buildings[j].getBuildingData().getTexture().Bounds.Intersects(Zombies[i].getTexture().Bounds))
-                    {
-                        float number = (float)Math.Atan2(Zombies[i].getDestination().Y - Zombies[i].getCurrentLocation().Y, Zombies[i].getDestination().X - Zombies[i].getCurrentLocation().X);
-                        number = MathHelper.ToDegrees(number);
-                        if (number % 45 > 22)
-                        {
-                            float zombieToMove = (Zombies[i].getDestination() - Zombies[i].getCurrentLocation()).Length();
-                            Zombies[i].setCurrentLocation(Zombies[i].getCurrentLocation() * new Vector2(-1,1));
-                        }
 
+                    if (Zombies[i].getDestination() != Vector2.Zero)
+                    {
+
+                        Zombies[i].setCurrentLocation(Zombies[i].getCurrentLocation() + zombieToMove * Zombies[i].getMoveSpeed());
+                        Zombies[i].setRotation((float)Math.Atan2(Zombies[i].getDestination().Y - Zombies[i].getCurrentLocation().Y, Zombies[i].getDestination().X - Zombies[i].getCurrentLocation().X));
+                    }
+                    else
+                    {
+                        //TODO: BEHAVIOUR WHEN THEY HAVE NOT SEEN THE SURVIVORS
                     }
                 }
+                catch (ArgumentOutOfRangeException) {/*it's because i delete the zombie and then reference...bad coding i know...but you can die in a fire.*/ }
             }
         }
         public void draw(SpriteBatch spriteBatch)
         {
             foreach (ZombieData zombie in Zombies)
             {
-                spriteBatch.Draw(zombie.getTexture(), zombie.getCurrentLocation(), null, Color.White, rotation, new Vector2(zombie.getTexture().Width / 2, zombie.getTexture().Height / 2),1f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(zombie.getTexture(), zombie.getCurrentLocation(), null, Color.White, zombie.getRotation(), new Vector2(zombie.getTexture().Width / 2, zombie.getTexture().Height / 2),1f, SpriteEffects.None, 0f);
                 //spriteBatch.Draw(zombie.getTexture(), zombie.getCurrentLocation(), Color.White);
             }
         }
